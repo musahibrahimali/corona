122b1625a617b3692b826da25fcb4134b536fa65
Musah Ibrahim Ali <musahibrahimali@gmail.com>
2020-09-14 04:56:16 -0700

    Initial commit

---
+++ android/app/src/main/kotlin/com/miastudiosinc/corona/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.miastudiosinc.corona
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/home_page.dart
@@ -0,0 +1,54 @@
+import '../widgets/home_page_widgets/home_categories.dart';
+import 'package:flutter/material.dart';
+import 'package:auto_size_text/auto_size_text.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      resizeToAvoidBottomPadding: false,
+      appBar: AppBar(
+        //TODO: Add a drawer window
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leading: IconButton(
+          onPressed: () {},
+          icon: const Icon(
+            Icons.menu,
+            color: Colors.black,
+          ),
+        ),
+        actions: <Widget>[
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              Icons.info_outline,
+              color: Colors.black,
+            ),
+          )
+        ],
+        centerTitle: true,
+        title: AutoSizeText(
+          "Covid-19 Tracker",
+          style: TextStyle(
+            fontSize: 20,
+            fontFamily: "Montserrat",
+            color: Colors.black,
+            fontWeight: FontWeight.w600,
+          ),
+          minFontSize: 14,
+          stepGranularity: 2,
+          maxLines: 1,
+        ),
+      ),
+      body: HomeCategories(),
+    );
+  }
+}
